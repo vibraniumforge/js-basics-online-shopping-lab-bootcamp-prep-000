1918562fae7cad717e02ca78a6bd574e043942c4
Greg Morgan <gregm9999@gmail.com>
2019-03-21 04:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ let newItem = {
+   "itemName": item,
+   "itemPrice": Math.floor(Math.random()*100)
+ }
+ getCart().push(newItem);
+ return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+  let ans = [];
+  if (getCart().length===1) {
+    for (let i=0; i< getCart().length; i++) {
+      ans.push(`In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`);
+      }
+      return ans.join(" ");
+    } else if (getCart().length >1){
+        ans.push(`In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`);
+    } else {
+      return "Your shopping cart is empty.";
+    }
+  }
+
 
 function total() {
   // write your code here
