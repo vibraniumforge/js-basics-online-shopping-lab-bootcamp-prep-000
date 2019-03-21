1b9340209e00cc18ec46ce32d502f3bbcf07dd38
Greg Morgan <gregm9999@gmail.com>
2019-03-21 04:27:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
+    const total = total();
     setCart({});
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}`
+    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}`
   }
 }
