a527d7cf31981dbf6f9f68c65ecb710f6fd18dda
Greg Morgan <gregm9999@gmail.com>
2019-03-21 04:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
     }
     } else if (getCart().length >1){
       for (let i=0; i< getCart().length-1; i++) {
-        ans.push(`In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice},`);
+        ans.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice},`);
       }
         ans.push(`and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}.`)
     } else {
